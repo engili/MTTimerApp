9fa70bbf1cac01051e7ddc773698cbf81d4caaa4
engili <lmt.coding@gmail.com>
2018-11-25 11:40:47 +0800

    first init

---
+++ MTTimerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MTTimerApp
+//
+//  Created by Mengtian Li on 2018/11/3.
+//  Copyright © 2018 Mengtian Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
